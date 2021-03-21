39dbb8dd9349e58a14e6fd40a28fd20cd1059c07
deempa <62642034+deempa@users.noreply.github.com>
2021-03-21 15:56:11 +0200

    added login and register

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const app = express();
+app.use(express.static("public"));
 
 app.get("/", function(req, res){
-    var ip = require('ip');
-    res.send(ip.address());
+    res.sendFile(__dirname + "/index.html")
+});
+
+app.get("/register", function(req, res){
+    res.sendFile(__dirname + "/register.html")
+});
+
+app.get("/login", function(req, res){
+    res.sendFile(__dirname + "/login.html")
 });
 
 app.listen(3000, function(){
-    console.log("listening");
+    console.log("listening")
 });
